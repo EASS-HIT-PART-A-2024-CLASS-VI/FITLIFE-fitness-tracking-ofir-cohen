6d43b50b3cbae200649d3cab6d97559ca37dbd60
ofiz <ofircohen599@gmail.com>
2024-12-28 20:04:16 +0200

    apply unit tests

--- backend/app/unit_tests.py
+++ backend/app/unit_tests.py
@@ -1,4 +1,10 @@
 from fastapi.testclient import TestClient
+import sys
+import os
+
+# Add the root directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
 
 client = TestClient(app)
