37ce1544cdccc229a1b853ef70950b4e49955fb1
ofiz <ofircohen599@gmail.com>
2025-01-02 01:45:24 +0200

    update src files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import UserManagement from './components/UserManagement';
 import WorkoutTracker from './components/WorkoutTracker';
@@ -12,16 +11,29 @@ import TrainingPrograms from './components/TrainingPrograms';
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/users" element={<UserManagement />} />
-        <Route path="/workouts" element={<WorkoutTracker />} />
-        <Route path="/nutrition" element={<NutritionTracker />} />
-        <Route path="/weight" element={<WeightTracker />} />
-        <Route path="/calories" element={<CalorieRecommendations />} />
-        <Route path="/training-programs" element={<TrainingPrograms />} />
-      </Routes>
+      <header>
+        <h1>Welcome to FITLIFE</h1>
+      </header>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/user-management">User Management</Link>
+        <Link to="/workout-tracker">Workout Tracker</Link>
+        <Link to="/nutrition-tracker">Nutrition Tracker</Link>
+        <Link to="/weight-tracker">Weight Tracker</Link>
+        <Link to="/calorie-recommendations">Calorie Recommendations</Link>
+        <Link to="/training-programs">Training Programs</Link>
+      </nav>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/user-management" element={<UserManagement />} />
+          <Route path="/workout-tracker" element={<WorkoutTracker />} />
+          <Route path="/nutrition-tracker" element={<NutritionTracker />} />
+          <Route path="/weight-tracker" element={<WeightTracker />} />
+          <Route path="/calorie-recommendations" element={<CalorieRecommendations />} />
+          <Route path="/training-programs" element={<TrainingPrograms />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
